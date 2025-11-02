69039a818ced8da221a77d01092f62e515202e25
Kiryl Zuyeu <kirylzuyeuimh@gmail.com>
2025-11-02 14:27:50 +0100

    fead: handle start command of project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('start entry file!!!')
+const userName = process.argv.slice(2)[0].replace('--username=', '');
+
+const greating = () => {
+    console.log(`Welcome to the File Manager, ${userName}!`);
+}
+
+greating();
